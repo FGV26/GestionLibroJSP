00da3903950f5d76ea78027dbbd09d6ab09192b7
FGV26 <145302993+FGV26@users.noreply.github.com>
2025-07-25 06:05:35 -0500

    Creacion de Base de datos
    USE libro;
    CREATE TABLE Libro (
        id_libro VARCHAR(20) PRIMARY KEY,
        titulo VARCHAR(100) NOT NULL,
        autor VARCHAR(100) NOT NULL
    );
    
    CREATE TABLE Socio (
        id_socio VARCHAR(20) PRIMARY KEY,
        nombre VARCHAR(100) NOT NULL,
        correo VARCHAR(100) NOT NULL
    );
    
    CREATE TABLE Prestamo (
        id_prestamo VARCHAR(20) PRIMARY KEY,
        id_libro VARCHAR(20) NOT NULL,
        id_socio VARCHAR(20) NOT NULL,
        fecha_prestamo DATE NOT NULL,
        fecha_devolucion DATE NOT NULL,
        fecha_retorno DATE,
    
        FOREIGN KEY (id_libro) REFERENCES Libro(id_libro),
        FOREIGN KEY (id_socio) REFERENCES Socio(id_socio)
    );

---
+++ src/main/java/Dao/IPrestamoDao.java
@@ -0,0 +1,14 @@
+package Dao;
+
+import Entity.Prestamo;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface IPrestamoDao {
+
+    // Implementar metodos necesarios para la interfaz IPrestamoDao
+    boolean addPrestamo(Prestamo prestamo) throws SQLException;
+    List<Prestamo> allPrestamos() throws SQLException;
+}
